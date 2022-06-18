b748c6a7bc6d625dfd13dfbf049584f07c6fd08e
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-18 12:09:09 +0200

    I have also made the 404 that the page is not found

--- src/PAGES/404/NotFound.jsx
+++ src/PAGES/404/NotFound.jsx
@@ -1,3 +1,11 @@
+import Navbar from "../../COMPONENTS/Navbar/Navbar";
 export default function PageNotFound() {
-  return <div className="text-red-400">404 Page Not Found</div>;
+  return (
+    <div className="w-[100%] h-[100vh] flex flex-col gap-1">
+      <Navbar />
+      <div className="h-[95%] w-[100%] flex items-center justify-center">
+        <div className="text-red-500 text-[2em]"> 404 Page not found!</div>
+      </div>
+    </div>
+  );
 }
